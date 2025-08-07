673af536e83aec69581c0e27d242b2d5882837c3
petr-plihal <petaplihal@seznam.cz>
2025-08-07 23:44:31 +0200

    chore: add simple analysis with plotting, aggregation, cmd report

--- app/scripts/main.py
+++ app/scripts/main.py
@@ -1,10 +1,39 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-from csv_loader import csv_loader
+from loading.csv_loader import csv_loader
 
 def main():
-    loader = csv_loader("Vypis_354650030277_20240401_20250401.csv")
+
+    # 1. Data Collection (Acquisition) & Data Cleaning (Preprocessing)
+    loader = csv_loader("app/scripts/test_data/Vypis_354650030277_20240115_20250710.csv")
     data = loader.get_dataframe()
+
+    # 2. Data Transformation (Structuring)
+    # Group data by years and months
+    month_groups = data.groupby([
+        data["date"].dt.year.rename("year"), 
+        data["date"].dt.month.rename("month"),
+        ])
+
+    # 3. Categorization (Labeling) & Exploratory Data Analysis (EDA)
+    monthly_income = month_groups.apply(lambda x: x[x['amount'] > 0]['amount'].sum())
+    monthly_expenses = month_groups.apply(lambda x: x[x['amount'] < 0]['amount'].sum())
+    monthly_net = month_groups['amount'].sum()
+    
+    monthly_analysis_df = pd.DataFrame({
+        'income': monthly_income,
+        'expenses': monthly_expenses,
+        'net': monthly_net,
+        'transaction_count': month_groups['amount'].count()
+    })
+
+    # 4. Visualization, Reporting
+    print("Monthly summary:")
+    print(monthly_analysis_df)
+
+    monthly_analysis_df.plot()
+    plt.show()
 
 if __name__ == "__main__":
     main()
