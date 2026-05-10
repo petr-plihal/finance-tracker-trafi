03b8969a12328714142ee1220ebdf011e1ed7470
petr-plihal <petaplihal@seznam.cz>
2026-05-10 22:49:48 +0200

    test: add reusable fixtures for creating and passing sqlite in-memory database to the app and creating database session

--- tests/test_statement_manager.py
+++ tests/test_statement_manager.py
@@ -1,6 +1,8 @@
 """Unit tests for StatementManager class"""
 import pytest
 
+from app.app import app as _app, db as _db
+
 from app.exceptions import StatementFileNotFoundError, StatementFileEmptyError
 from app.models.statement_manager import StatementManager
 
@@ -22,3 +24,29 @@ class TestStatementManager:
 
     def test_csv_simple_records(self):
         StatementManager("tests/data/csv_simple_records.csv")
+@pytest.fixture
+def app():
+    _app.config.update({
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+    })
+
+    with _app.app_context():
+        _db.create_all()
+        yield _app
+        _db.drop_all()
+
+@pytest.fixture
+def session(app):
+    """Creates a new database session for a test."""
+    connection = _db.engine.connect()
+    transaction = connection.begin()
+
+    # Bind the session to the connection
+    session = _db.session
+
+    yield session
+
+    # Roll back everything after the test is done
+    session.remove()
+    transaction.rollback()
+    connection.close()
