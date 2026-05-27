bf98d99f0180b4bed88717b36ae1cd66e15df360
petr-plihal <petaplihal@seznam.cz>
2026-05-27 23:18:39 +0200

    test: add fixtures for creating testing Account, Transaction and Currency records

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -11,6 +11,11 @@ import pytest
 
 from app.app import app as _app, db as _db
 
+from app.models.database import Currency, Account, Transaction
+
+from datetime import datetime
+from decimal import Decimal
+
 @pytest.fixture
 def app():
     _app.config.update({
@@ -24,7 +29,11 @@ def app():
 
 @pytest.fixture
 def session(app):
-    """Creates a new database session for a test."""
+    """
+    Creates a new database session for a test.
+    
+    Importantly, it rolls back 
+    """
     connection = _db.engine.connect()
     transaction = connection.begin()
 
@@ -37,3 +46,47 @@ def session(app):
     session.remove()
     transaction.rollback()
     connection.close()
+
+@pytest.fixture
+def insert_testing_currency(session) -> Currency:
+    """Adds a testing currency record to database."""
+
+    currency_record = Currency(
+        name = "TEST_CURRENCY",
+        sign = "test_cur",
+        code = "TST",
+    )
+    session.add(currency_record)
+    session.flush() # commit() cannot be used, since that would break the rollback() in session fixture
+    return currency_record
+
+@pytest.fixture
+def insert_testing_account(session) -> Account:
+    """Adds a testing account record to database."""
+
+    account_record = Account(
+        name = "test_account",
+        bank_name = "test_bank",
+        account_number = "12345678/9012",
+    )
+    session.add(account_record)
+    session.flush() # commit() cannot be used, since that would break the rollback() in session fixture
+    return account_record
+
+@pytest.fixture
+def insert_testing_transaction(session, insert_testing_currency, insert_testing_account) -> Transaction:
+    transaction_record = Transaction(
+        amount = Decimal(10.0),
+        date = datetime(year = 2020, month = 9, day = 9),
+        contra_account_number = "81880926/2700",
+        contra_account_name = "81880926/2700",
+        my_description = None,
+        message = None,
+
+        account = insert_testing_account,
+        currency = insert_testing_currency,
+        category = None,
+    )
+    stmt = session.add(transaction_record)
+    session.flush() # commit() cannot be used, since that would break the rollback() in session fixture
+    return transaction_record
